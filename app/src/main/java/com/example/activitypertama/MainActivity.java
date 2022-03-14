66c392938589c2f1ad8ba1f022ca0ddd1b73e8a3
MuhammadIrsyad06 <muhammadirsyad764@gmail.com>
2022-03-14 23:44:03 +0700

    Update Act 4

--- app/src/main/java/com/example/activitypertama/MainActivity.java
+++ app/src/main/java/com/example/activitypertama/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.activitypertama;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -81,5 +84,22 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Method untuk menampilkan menu
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.mnDaftar)
+        {
+            //Method untuk memanggil activity "MenuActivity"
+            Intent i = new Intent(getApplicationContext(),MenuActivity.class);
+            startActivity(i);
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
